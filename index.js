5baae9b2be41cb0ef1682ffba265dc99b53482a9
Prem Khandelwal <prem.dkhandelwal@gmail.com>
2023-06-23 14:17:10 +0530

    Production ready

--- index.js
+++ index.js
@@ -1,36 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
-const app = express();
-const port = 4000;
 const cors = require('cors');
 require('dotenv').config();
 
-// Middleware to parse JSON and URL-encoded bodies
+const app = express();
+const port = process.env.PORT || 4000;
 
-
-const corsOptions ={
-    origin:'http://localhost:3000', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
-app.use(cors({
-    credentials: true,
-    preflightContinue: true,
-    methods: ['GET', 'POST', 'PUT', 'PATCH' , 'DELETE', 'OPTIONS'],
-    origin: true
-}));
-
+// Middleware
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-// Middleware to parse JSON and URL-encoded bodies
 
-// POST endpoint to handle form data
+// POST route for form submission
 app.post('/submitform', async (req, res) => {
   const formData = req.body;
 
   try {
-    // Send email with the form data
     await sendEmail(formData);
     res.status(200).json({ message: 'Form submitted successfully' });
   } catch (error) {
@@ -41,30 +27,26 @@ app.post('/submitform', async (req, res) => {
 
 // Function to send email
 async function sendEmail(formData) {
-  // Create a transporter using Gmail's SMTP settings
   const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 587,
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
     secure: false,
     auth: {
-      user: 'prem.dkhandelwal@gmail.com', // Replace with your Gmail email address
-      pass: process.env.PASSWORD, // Replace with your Gmail password or app-specific password
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASSWORD,
     },
   });
 
-  // Create the email message
   const message = {
-    from: 'prem.dkhandelwal@gmail.com',
-    to: formData.emailId, // Replace with the recipient's email address
+    from: process.env.EMAIL_USER,
+    to: formData.emailId,
     subject: 'Collaboration Form Submission',
     text: JSON.stringify(formData, null, 2),
   };
 
-  // Send the email
   await transporter.sendMail(message);
   console.log('Email sent');
 }
-
 
 // Start the server
 app.listen(port, () => {
