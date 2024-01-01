8f6beebb9e09ae02c082723a40ad49e24e85de10
premwebber <prem.khandelwal@webberec.com>
2024-01-01 18:23:54 +0530

    Added access control allow origin in header request

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 4000;
 
+
+app.use(function (req, res, next) {
+  //Enabling CORS
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+    next();
+  });
 // Middleware
 app.use(cors({
   origin: 'https://premkhandelwal.github.io', // Add your front-end domain
