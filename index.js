310cd91a9915ae4d8f43b7f7a269aa98d94a1073
premwebber <prem.khandelwal@webberec.com>
2024-01-01 18:18:04 +0530

    Changed secure to true

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ async function sendEmail(formData) {
   const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
     port: process.env.EMAIL_PORT,
-    secure: false,
+    secure: true,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASSWORD,
