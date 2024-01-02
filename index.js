3858a15d24047a83dfc4a742a7beffb4490ba5b2
premwebber <prem.khandelwal@webberec.com>
2024-01-02 19:56:08 +0530

    Removed unnecessary headers

--- index.js
+++ index.js
@@ -8,13 +8,7 @@ const app = express();
 const port = process.env.PORT || 4000;
 
 
-app.use(function (req, res, next) {
-  //Enabling CORS
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
-    next();
-  });
+
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
